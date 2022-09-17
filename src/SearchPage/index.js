8c8b11e2baab452421f1cf52dcf7888a78563c72
K02D <kirondeb02@gmail.com>
2022-09-17 14:59:22 -0400

    Add modal for students

--- src/SearchPage/index.js
+++ src/SearchPage/index.js
@@ -2,14 +2,23 @@ import { TextField , Grid, Typography, Paper, IconButton } from "@mui/material";
 import { useState } from "react";
 import { Stack } from "@mui/material";
 import { Button } from "@mui/material";
-import List from '@mui/material/List';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Modal } from "@mui/material";
+import { Box } from "@mui/material";
+import CustomModal from "../CustomModal";
 
 function SearchPage() {
     // const [courses, setCourses] = useState();
-    // const [studentList, setStudentList] = usetState();
-    const courses = [{id: 1, name: "course 1"}, {id: 2, name: "course 2"}, {id: 3, name: "course 3"}]
-    const studentList = [{courseName: 'Course 1', students: ['Astrid', 'Kiron', 'Mitra', 'William']},
-    {courseName: 'Course 2', students: ['John', 'Doe', 'Foo', 'Bar']}]
+    const courses = {1: {name: "Course 1", students: ['Astrid', 'Kiron', 'Mitra', 'William']}, 
+                    2: {name: "Course 2", students: ['John', 'Doe', 'Foo', 'Bar']}}
+    const [open, setOpen] = useState(false);
+    const [studentForModal, setStudentForModal] = useState();
+    const handleOpen = (studentName) => {
+        setStudentForModal(studentName)
+        setOpen(true)
+    };
+    const handleClose = () => setOpen(false);
+    
 
     // Find courses as user types in the TextField
     function handleTextChange(event) {
@@ -18,7 +27,14 @@ function SearchPage() {
 
     // Show students for course with the given id
     function handleShowStudents(id) {
-        console.log(id)
+        console.log(id);
+    }
+
+    function handleDeleteCourse(id) {
+        console.log(id);
+        // const newCourses = courses;
+        // delete newCourses[id];
+        // setCourses(newCourses);
     }
 
     return (
@@ -36,27 +52,34 @@ function SearchPage() {
                 <Grid item>
                     <Stack direction="column" spacing={2}>
                         {courses ? 
-                        (courses.map(({id, name}) => 
-                            <Button id={id} variant="contained" style={{width: "250px", textTransform: 'capitalize'}}
-                            onClick={(event) => handleShowStudents(event.target.id)}> 
-                                {name} 
-                            </Button>
+                        (Object.keys(courses).map((id) => 
+                            <Grid container>
+                                <Grid item xs={9}> 
+                                    <Button id={id} variant="contained" style={{width: "250px", textTransform: 'capitalize'}}
+                                    onClick={(event) => handleShowStudents(event.target.id)}> 
+                                        {courses[id].name} 
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={3} margin="auto">
+                                    <DeleteIcon onClick={() => handleDeleteCourse(id)}/>
+                                </Grid>
+                            </Grid>
                         )) 
                         : null}
                     </Stack>
                 </Grid>
             </Grid>
 
-            {studentList.map((courseInfo) => 
+            {Object.keys(courses).map((id) => 
             <Grid container item xs >
                 <Grid item xs>
                     <Typography variant="h4" align="center">
-                        {courseInfo.courseName}
+                        {courses[id].courseName}
                     </Typography>
                     <Stack direction="column" spacing={2} alignItems="center"> 
-                        {courseInfo.students ? 
-                        courseInfo.students.map((studentName) => 
-                        <IconButton >
+                        {courses[id].students ? 
+                        courses[id].students.map((studentName) => 
+                        <IconButton onClick={() => handleOpen(studentName)}>
                             <Paper style={{width: '250px'}}>
                                 {studentName}
                             </Paper>
@@ -65,6 +88,7 @@ function SearchPage() {
                         : null}
                     </Stack>
                 </Grid>
+                <CustomModal open={open} handleClose={handleClose} student={studentForModal}/>
             </Grid>
             )}
 
