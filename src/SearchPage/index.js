6278f26936d843c4ff0e565eda1fc65276a39367
K02D <kirondeb02@gmail.com>
2022-09-17 10:51:56 -0400

    Add course buttons

--- src/SearchPage/index.js
+++ src/SearchPage/index.js
@@ -1,14 +1,25 @@
 import { TextField , Grid } from "@mui/material";
-
-
+import { useState } from "react";
+import { Stack } from "@mui/material";
+import { Button } from "@mui/material";
 function SearchPage() {
-    // find courses as user types in the TextField
+    // const [courses, setCourses] = useState();
+    // const courses = [(1, "course 1"), (2, "course 2"), (3, "course 3")]
+    const courses = [{id: 1, name: "course 1"}, {id: 2, name: "course 2"}, {id: 3, name: "course 3"}]
+
+    // Find courses as user types in the TextField
     function handleTextChange(event) {
         console.log(event.target.value);
     }
+
+    // Show students for course with the given id
+    function handleShowStudents(id) {
+        console.log(id)
+    }
+
     return (
         <div>
-        <Grid container direction="column" style={{ marginLeft: '40px', marginTop: '40px' }}>
+        <Grid container direction="column" spacing='60px' style={{ marginLeft: '40px', marginTop: '40px' }}>
             <Grid item>
                 <TextField
                     id="outlined-basic"
@@ -18,8 +29,17 @@ function SearchPage() {
                     />
                 </Grid>
             <Grid item>
-                    test
-                </Grid>
+                <Stack direction="column" spacing={2}>
+                    {courses ? 
+                    (courses.map(({id, name}) => 
+                        <Button id={id} variant="contained" style={{width: "300px", textTransform: 'capitalize'}}
+                        onClick={(event) => handleShowStudents(event.target.id)}> 
+                            {name} 
+                        </Button>
+                    )) 
+                    : null}
+                </Stack>
+            </Grid>
 
             </Grid>
 
