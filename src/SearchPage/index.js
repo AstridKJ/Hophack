6c98504a31965411080c674d292eebd634f0b806
K02D <kirondeb02@gmail.com>
2022-09-17 10:25:49 -0400

    Add mui and textfield

--- src/SearchPage/index.js
+++ src/SearchPage/index.js
@@ -1,8 +1,29 @@
+import { TextField , Grid } from "@mui/material";
+
+
 function SearchPage() {
-  return (
-    <div>
-      Search page here
-    </div>
+    // find courses as user types in the TextField
+    function handleTextChange(event) {
+        console.log(event.target.value);
+    }
+    return (
+        <div>
+        <Grid container direction="column" style={{ marginLeft: '40px', marginTop: '40px' }}>
+            <Grid item>
+                <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    label="Search"
+                    onChange={handleTextChange}
+                    />
+                </Grid>
+            <Grid item>
+                    test
+                </Grid>
+
+            </Grid>
+
+        </div>
   );
 }
 
